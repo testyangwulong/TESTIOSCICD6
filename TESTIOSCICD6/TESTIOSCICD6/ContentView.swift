01fa58aadb4c9ea0a8811f950eccc41536ea145c
Wulong yang <yange@uniware.com.au>
2020-10-08 11:09:53 +1100

    Initial commit

---
+++ TESTIOSCICD6/TESTIOSCICD6/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TESTIOSCICD6
+//
+//  Created by yang wulong on 8/10/20.
+//  Copyright © 2020 yang wulong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
